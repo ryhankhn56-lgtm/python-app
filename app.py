d43abc5c38d3456e41907b4ff4e671190433e922
ryhankhn56-lgtm <ryhankhn56@gmail.com>
2025-10-25 19:33:32 +0500

    First Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+#Find more emojis here: https://pythonandvba.com/emojify/
+st.set_page_config(page_title="MY Webpage", page_icon=":tada:", layout="wide")
+
+#---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, Iam Rehan :wave:")
+    st.title("A Data Analyst From Pakistan")
+    st.write("I am passionate about finding python and VBA to more efficient and effective in business settings.")
+    st.markdown("[Learn More>](https://pythonandvba.com/grafly/)")       
+
+
+
